d2b3b9f2abe667196b6085cea7f7211f81afc23c
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-19 13:23:39 +0600

    some changes

--- solver.js
+++ solver.js
@@ -1,22 +1,31 @@
 const solutionDiv = document.getElementById("solution");
-solutionDiv.style.display = "none";
+solutionDiv.style.display = "none"; // Always hidden by default!
 
 function triggerSolution() {
+    // Getting coefficients
     const coefA = parseFloat(document.getElementById("coef-a").value);
     const coefB = parseFloat(document.getElementById("coef-b").value);
     const coefC = parseFloat(document.getElementById("coef-c").value);
-    const solutionText = document.getElementById("solution-text");
 
+    // Solution <p> tag
+    const solutionText = document.getElementById("solution-text");
     
-    if (!coefA || !coefB || !coefC) {
+    /* Making sure nothing is empty or invalid. Empty/invalid is NaN in JS */
+    if (isNaN(coefA) || isNaN(coefB) || isNaN(coefC)) {
+        if (solutionDiv.style.display === "block") {
+            solutionDiv.style.display = "none";
+        }
         return; // empty values are NaN
     }
 
     if (coefA === 0) {
         solutionText.innerHTML = "Please enter a nonzero coefficient for A!";
+
     } else {
+        // Discriminant before calculations
         let discriminant = (coefB * coefB) - (4 * coefA * coefC);
         if (discriminant < 0) {
+            // if I tried harder I could make it handle complex numbers but for now? nope
             solutionText.innerHTML = "".concat("The discriminant (", discriminant, ") is negative. There are complex roots.");
         } else if (discriminant === 0){
             let x = (-coefB + Math.sqrt(discriminant))/(2 * coefA);
